aac44930ad91fc395ad1aa4c24609769c3c6e0b6
Scogur <113353238+Scogur@users.noreply.github.com>
2024-08-30 20:22:48 +0700

    added queries and magic strings

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,50 @@
 import express, { json, Request, Response } from 'express';
 
+const THIS_APP_PORT = 2000;
+const REP_APP_LINK = 'http://localhost:3000';
+
 const app = express();
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || THIS_APP_PORT;
+
 
 
 startServer(app);
 
-//getReport(getRequest());
-
 
 function startServer(_app : express.Application){
   _app.get('/', async (req: Request, res: Response) => {
-    let report = await getReport(getRequest())
+    
+
+    let th = JSON.parse(JSON.stringify(req.query.th));
+    console.log(th);
+    let report = await getReport(getRequest(th));
     //console.log(report);
-    //res.sendFile(report);
     res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     res.send(Buffer.from(report));
   });
-  
   _app.listen(port, () => {
-      console.log(`Server is running at  http://localhost:2000/`);
+      console.log(`Server is running at  http://localhost:${THIS_APP_PORT}/`);
+      console.log(`example request http://localhost:${THIS_APP_PORT}/?th=[%22name%22,%22occupation%22]`);
     });
 }
 
 async function getReport(req: RequestInfo){
-  //let data: string | undefined;
   let data;
-  //let blob;
   await fetch(req)
   .then(response => response.arrayBuffer())
   .then(res => {
     data = res as ArrayBuffer;
   });
-
-
   return data as unknown as ArrayBuffer;
 }
 
-
-
-
-function getRequest(){
-  return new Request(`http://localhost:3000/rep`, {
+function getRequest(th: string[]){
+  return new Request(`${REP_APP_LINK}/rep`, {
     method: 'POST',
     body: JSON.stringify({
         name: 'Nick',
         tablename: 'report',
-        tableheads: ['name', 'occupation', 'phonenumber'],
+        tableheads: th,
     }),
     headers: {
         'Content-Type': 'application/json',
